8e1b13b04b2f25af1e2ea3c2120cbe03489f4ca9
Nguyen Quang Bich HE173220 <bichnqhe173220@fpt.edu.vn>
2024-07-18 22:23:06 +0700

    create semester DAO

---
+++ CourseManagement/SemesterManagement.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BusinessObjects;
+using DataAccessLayer;
+namespace CourseManagement
+{
+    /// <summary>
+    /// Interaction logic for SemesterManagement.xaml
+    /// </summary>
+    public partial class SemesterManagement : Window
+    {
+        public SemesterManagement()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+
+        }
+
+        private void CodeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void YearComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void StartDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void EndDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void Load_Semester()
+        {
+            SemestersDAO _semesterDao = new SemestersDAO();
+            List<Semester> semesters = _semesterDao.Load_Semester(null,-1,null,null);
+        }
+    }
+}
---
+++ DataAccessLayer/SemestersDAO.cs
@@ -0,0 +1,35 @@
+﻿using BusinessObjects;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class SemestersDAO
+    {
+        public List<Semester> Load_Semester(string code, int year, DateOnly startDate, DateOnly endDate)
+        {
+            List<Semester> semesters = new List<Semester>();
+            try
+            {
+                using (var _dbcontext = new CourseManagementDbContext())
+                {
+                    IQueryable<Semester> query = _dbcontext.Semesters;
+                    if (!code.IsNullOrEmpty()) { query = query.Where(s => s.Code == code); }
+                    if (year != -1) { query = query.Where(s => s.Year == year); }
+                    if( startDate != null) { query = query.Where(s => s.BeginDate == startDate); }
+                    if(endDate != null) { query = query.Where(s => s.EndDate == endDate); }
+                    semesters = query.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return semesters;
+        }
+    }
+}
