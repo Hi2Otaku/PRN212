230fa40c3a5c0e3a20b0c82c77617b996f2824be
PahmNogcHa2003 <pnha03dev.it@gmail.com>
2024-07-18 23:07:43 +0700

    Create folder CoursesManage and complete CourseManage.xaml

---
+++ DataAccessLayer/AssessmentDAO.cs
@@ -0,0 +1,51 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class AssessmentDAO
+    {
+        private readonly CourseManagementDbContext _context;
+
+        public AssessmentDAO(CourseManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Assessment> GetAllAssessments()
+        {
+            return _context.Assessments.ToList();
+        }
+
+        public Assessment GetAssessmentById(int id)
+        {
+            return _context.Assessments.Find(id);
+        }
+
+        public void AddAssessment(Assessment assessment)
+        {
+            _context.Assessments.Add(assessment);
+            _context.SaveChanges();
+        }
+
+        public void UpdateAssessment(Assessment assessment)
+        {
+            _context.Assessments.Update(assessment);
+            _context.SaveChanges();
+        }
+
+        public void DeleteAssessment(int id)
+        {
+            var assessment = _context.Assessments.Find(id);
+            if (assessment != null)
+            {
+                _context.Assessments.Remove(assessment);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/EnrollmentDAO.cs
@@ -0,0 +1,52 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class EnrollmentDAO
+    {
+        private readonly CourseManagementDbContext _context;
+
+        public EnrollmentDAO(CourseManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Enrollment> GetAllEnrollments()
+        {
+            return _context.Enrollments.ToList();
+        }
+
+        public Enrollment GetEnrollmentById(int id)
+        {
+            return _context.Enrollments.Find(id);
+        }
+
+        public void AddEnrollment(Enrollment enrollment)
+        {
+            _context.Enrollments.Add(enrollment);
+            _context.SaveChanges();
+        }
+
+        public void UpdateEnrollment(Enrollment enrollment)
+        {
+            _context.Enrollments.Update(enrollment);
+            _context.SaveChanges();
+        }
+
+        public void DeleteEnrollment(int id)
+        {
+            var enrollment = _context.Enrollments.Find(id);
+            if (enrollment != null)
+            {
+                _context.Enrollments.Remove(enrollment);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
+
