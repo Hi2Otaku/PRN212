408d6ddc4490dca1f19a1a48279c5bea1f9b9630
Hi2Otaku <hi2otaku@gmail.com>
2024-07-19 16:23:21 +0700

    Commit

---
+++ DataAccessLayer/AccountMemberDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+
+namespace DataAccessLayer
+{
+    public class AccountMemberDAO
+    {
+        private static AccountMember GetAccountMemberByName(String name)
+        {
+            AccountMember accountMember = new AccountMember();
+            CourseManagementDbContext db = new CourseManagementDbContext();
+            accountMember = db.AccountMembers.Find(name);
+            return accountMember;
+        }
+    }
+}
